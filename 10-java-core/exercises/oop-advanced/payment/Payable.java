14b24dfa89339b55081ed41060c994f4341080a2
akhila28k <146351769+akhila28k@users.noreply.github.com>
2026-01-14 13:39:10 +0530

    payment files added

---
+++ 10-java-core/exercises/oop-advanced/payment/Payable.java
@@ -0,0 +1,8 @@
+public interface Payable {
+
+    double getPaymentAmount();
+
+    default void printPaymentInfo() {
+        System.out.println("Payment Amount: $" + getPaymentAmount());
+    }
+}
