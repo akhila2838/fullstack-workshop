03fc3fe4d17e447b69391982367807bed34d38b7
akhila28k <146351769+akhila28k@users.noreply.github.com>
2026-01-14 21:52:46 +0530

    Exercise 3.3

---
+++ 10-java-core/exercises/collections-streams/optional/Address.java
@@ -0,0 +1,18 @@
+
+package Optional;
+
+
+import java.util.Optional;
+
+public class Address {
+
+    private String apartment;
+
+    public Address(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public Optional<String> getApartment() {
+        return Optional.ofNullable(apartment);
+    }
+}
