4c211bf57cb420e773f6c04a16eac062ccadce06
akhila28k <146351769+akhila28k@users.noreply.github.com>
2026-01-14 16:15:19 +0530

    exercise 2.4

---
+++ 10-java-core/exercises/oop-advanced/registration/DuplicateUserException.java
@@ -0,0 +1,9 @@
+
+package registration;
+
+public class DatabaseException extends Exception {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
---
+++ 10-java-core/exercises/oop-advanced/registration/ValidationException.java
@@ -0,0 +1,10 @@
+
+package registration;
+
+
+public class ValidationException extends Exception {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
